2c095c2a2072bbc1ff7d44b5e9609c9ac3ae3f4e
lir <lir-567@rambler.ru>
2019-07-13 08:34:58 +0500

    Stage 3 completed: -Added multithreading; -Instead of file name folder name required as first parameter; -Always use KMP as substring search algorithm.

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -1,85 +1,45 @@
 package analyzer;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
-    public static void main(String[] args) {
-        if (args.length != 4) throw new InputMismatchException("Wrong amount of arguments!");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        if (args.length != 3) throw new InputMismatchException("Wrong amount of arguments!");
 
-        String alg = args[0];
-        HashMap<String, String> dict = new HashMap<>();
-        dict.put(args[2], args[3]);
-        long t = System.nanoTime();
-        try (InputStream inputStream = new FileInputStream(args[1])) {
-            String content = new String(inputStream.readAllBytes());
-            for (String s :
-                    dict.keySet()) {
-                if (alg.equals("--naive")) {
-                    if (content.contains(s)) {
-                        System.out.println(dict.get(s));
-                        System.out.println("It took "+ (System.nanoTime()-t)/1000000000.0 + " seconds");
-                        return;
-                    }
-                } else if (alg.equals("--KMP")) {
-                    if(KMPSearch(content, s)) {
-                        System.out.println(dict.get(s));
-                        System.out.println("It took "+ (System.nanoTime()-t)/1000000000.0 + " seconds");
-                        return;
-                    }
-                }
-            }
-        } catch (IOException | OutOfMemoryError e) {
-            System.out.println("Error while openning file: " + args[0] + "\n" + e.getMessage());
-        }
-        System.out.println("Unknown file type");
-        System.out.println("It took "+ (System.nanoTime()-t)/1000000000.0 + " seconds");
-    }
 
-    private static int[] prefixFunction(String str) {
-
-        int[] prefixFunc = new int[str.length()];
-
-        for (int i = 1; i < str.length(); i++) {
-
-            int j = prefixFunc[i - 1];
-
-            while (j > 0 && str.charAt(i) != str.charAt(j)) {
-                j = prefixFunc[j - 1];
-            }
-
-            if (str.charAt(i) == str.charAt(j)) {
-                j += 1;
-            }
-
-            prefixFunc[i] = j;
-        }
-
-        return prefixFunc;
-    }
-
-    private static boolean KMPSearch(String text, String pattern) {
-
-        int[] prefixFunc = prefixFunction(pattern);
-        int j = 0;
-
-        for (int i = 0; i < text.length(); i++) {
-
-            while (j > 0 && text.charAt(i) != pattern.charAt(j)) {
-                j = prefixFunc[j - 1];
-            }
-
-            if (text.charAt(i) == pattern.charAt(j)) {
-                j += 1;
-            }
-
-            if (j == pattern.length()) {
-                return  true;
+        String type = args[2];
+        String pattern = args[1];
+        File directory = new File(args[0]);
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        HashMap<String, Future<Boolean>> fileCheckers = new HashMap<>();
+        for (File f : Objects.requireNonNull(directory.listFiles())) {
+            try (InputStream inputStream = new FileInputStream(f)) {
+                String content = new String(inputStream.readAllBytes());
+                Future<Boolean> search = executor.submit(new KMPSearch(content, pattern));
+                fileCheckers.put(f.getName(), search);
+            } catch (IOException | OutOfMemoryError e) {
+                System.out.println("Error while opening file: " + args[0] + "\n" + e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-
-        return false;
+        var filenames = (String[]) fileCheckers.keySet().toArray(new String[0]);
+        Arrays.sort(filenames);
+        for (String fileName : filenames) {
+            if (fileCheckers.get(fileName).get()) {
+                System.out.println(fileName + ": " + type);
+            } else {
+                System.out.println(fileName + ": " + "Unknown file type");
+            }
+        }
+        executor.shutdown();
     }
-
 }
