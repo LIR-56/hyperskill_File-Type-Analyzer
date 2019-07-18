9f10f88a910cacd181051723d41be62beffe4633
lir <lir-567@rambler.ru>
2019-07-18 13:55:38 +0500

    Moved reading of pattern DB file to separate function.

--- File Type Analyzer/task/src/analyzer/Main.java
+++ File Type Analyzer/task/src/analyzer/Main.java
@@ -11,21 +11,9 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         if (args.length != 2) throw new InputMismatchException("Wrong amount of arguments!");
 
-        File patternsBase = new File(args[1]);
         ArrayList<Pair<String, String>> patterns;
-        try (InputStream inputStream = new FileInputStream(patternsBase)) {
-            String[] parsedContent = new String(inputStream.readAllBytes()).split("\n");
-
-            patterns = new ArrayList<>(parsedContent.length);
-            for (int i = parsedContent.length-1; i >= 0; i--) {
-                String[] parsedRaw = parsedContent[i].split(";");
-                patterns.add(new Pair<>(parsedRaw[1].substring(1, parsedRaw[1].length() - 1),
-                        parsedRaw[2].substring(1, parsedRaw[2].length() - 1)));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+        patterns = readPatterns(new File(args[1]));
+        if (patterns == null) return;
 
         File directory = new File(args[0]);
         ExecutorService executor = Executors.newFixedThreadPool(10);
@@ -42,12 +30,32 @@ public class Main {
                 e.printStackTrace();
             }
         }
+
         var filenames = (String[]) fileCheckers.keySet().toArray(new String[0]);
         Arrays.sort(filenames);
         for (String fileName : filenames) {
             System.out.println(fileName + ": " + fileCheckers.get(fileName).get());
         }
         executor.shutdown();
+    }
+
+
+    private static ArrayList<Pair<String, String>> readPatterns(File patternsDataBase) {
+        ArrayList<Pair<String, String>> patterns;
+        try (InputStream inputStream = new FileInputStream(patternsDataBase)) {
+            String[] parsedContent = new String(inputStream.readAllBytes()).split("\n");
+
+            patterns = new ArrayList<>(parsedContent.length);
+            for (int i = parsedContent.length-1; i >= 0; i--) {
+                String[] parsedRaw = parsedContent[i].split(";");
+                patterns.add(new Pair<>(parsedRaw[1].substring(1, parsedRaw[1].length() - 1),
+                        parsedRaw[2].substring(1, parsedRaw[2].length() - 1)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return patterns;
     }
 }
 
